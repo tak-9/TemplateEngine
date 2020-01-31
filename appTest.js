356a768fc06f32b48ac3ff0f4c38b8a5f7166a8c
Takuji Okubo <takuji.okubo@gmail.com>
2020-01-31 17:19:55 +1100

    updated readme

--- appTest.js
+++ appTest.js
@@ -5,13 +5,13 @@ const generateHTML = require("./generateHTML");
 const path = require("path");
 
 // Test Data
-var m = new Manager("Bar", 1, "test@test.com", 100);
-var e1 = new Engineer("Eng1", 2, "test1@test.com", "githubname1");
-var e2 = new Engineer("Eng2", 3, "test2@test.com", "githubname2");
-var e3 = new Engineer("Eng3", 4, "test3@test.com", "githubname3");
-var i1 = new Intern("Int1", 5, "ddst@test.com", "university");
-var i2 = new Intern("Int2", 6, "ddfadasdst@test.com", "uni123");
-var i3 = new Intern("Int3", 7, "int3@test.com", "uni12345");
+var m = new Manager("Djokovic", 1, "test@test.com", 100);
+var e1 = new Engineer("Federer", 2, "test1@test.com", "githubname1");
+var e2 = new Engineer("Nadal", 3, "test2@test.com", "githubname2");
+var e3 = new Engineer("Wawrinka", 4, "test3@test.com", "githubname3");
+var i1 = new Intern("Kyrgios", 5, "ddst@test.com", "university");
+var i2 = new Intern("Intern1", 6, "ddfadasdst@test.com", "uni123");
+var i3 = new Intern("Intern2", 7, "int3@test.com", "uni12345");
 var team = [m, e1, e2, e3, i1, i2, i3];
 
 var outputFilenameTable = path.resolve(__dirname  + "/output/table.html");
